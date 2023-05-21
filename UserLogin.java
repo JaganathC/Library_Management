480cd446b38ad4261ff2f8924eaa9c6348fd2ad1
CHOWDAM JAGANATH <123239098+JaganathC@users.noreply.github.com>
2023-05-21 20:48:19 +0530

    Create UserLogin.java

---
+++ UserLogin.java
@@ -0,0 +1,10 @@
+JAGANATH C/bcs123/
+Test A/abcd/
+Messi/leo/
+TestUser A B/xyz1234/
+TestUser C D/lmno/
+TestUser E F/xyz1234/
+TestUser X Y Z/pqrs/
+TestUser X Y Z/pqrs/
+TestUser X Y Z/xyz/
+TestUser F G/bcd/
